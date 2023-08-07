babeafde740b08c14fada19cade4e419bb0a107f
LoganNissenson <logannissenson@gmail.com>
2023-08-07 04:32:36 +0000

    Setting up problem 2.4

---
+++ Problem 2.4.py
@@ -0,0 +1,6 @@
+# Partition: Write code to partition a linked list around a value x, sucn that all nodes less than x come before all nodes greater than or equal to x
+# IMPORTANT: The partition element x can appear anywhere in the "Right partition"; it does not need to appear between the left and right partitions
+# The additional spacing in the example indicates teh partition. Yes, the output below is one of many valid outputs
+#
+#Input 3 -> 5 -> 8 -> 5-> 10 -> 2 -> 1 (partition 5)
+# Output 3 ->1 -> 2 -> 10 -> 5 -> 5 -> 8 
